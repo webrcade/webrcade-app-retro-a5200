2a9064a65b32a45a2c39b66b34f35fe183c7be2c
raz0red <raz0red@gmail.com>
2026-01-06 22:27:42 -0800

    bilinear modes

--- src/pause/settings.js
+++ src/pause/settings.js
@@ -29,8 +29,8 @@ export class A5200SettingsEditor extends Component {
 
 
     const values = {
-      origBilinearMode: emulator.getPrefs().isBilinearEnabled(),
-      bilinearMode: emulator.getPrefs().isBilinearEnabled(),
+      origBilinearMode: emulator.getPrefs().getBilinearMode(),
+      bilinearMode: emulator.getPrefs().getBilinearMode(),
       origScreenSize: emulator.getPrefs().getScreenSize(),
       screenSize: emulator.getPrefs().getScreenSize(),
     }
@@ -66,6 +66,7 @@ export class A5200SettingsEditor extends Component {
       content: (
         <AppDisplaySettingsTab
           emulator={emulator}
+          isBilinearMode={true}
           isActive={tabIndex === tab}
           showOnScreenControls={showOnScreenControls}
           setFocusGridComps={setFocusGridComps}
@@ -100,8 +101,7 @@ export class A5200SettingsEditor extends Component {
 
           let change = false;
           if (values.origBilinearMode !== values.bilinearMode) {
-            emulator.getPrefs().setBilinearEnabled(values.bilinearMode);
-            emulator.updateBilinearFilter();
+            emulator.getPrefs().setBilinearMode(values.bilinearMode);
             change = true;
           }
           if (values.origScreenSize !== values.screenSize) {
@@ -115,6 +115,7 @@ export class A5200SettingsEditor extends Component {
 
           // Set the shader
           await this.shaderService.setShader(values.shaderId);
+          emulator.updateBilinearFilter();
 
           onClose();
         }}

58212a7d93db4410010137e43eeabb8a6fca1101
raz0red <raz0red@gmail.com>
2026-05-10 23:57:39 -0700

    achieve

--- src/pause/index.js
+++ src/pause/index.js
@@ -6,10 +6,12 @@ import { GamepadControlsTab, KeyboardControlsTab } from './controls';
 import { A5200SettingsEditor } from './settings';
 
 import {
+  AchievementsScreen,
   BoltWhiteImage,
   CheatsSettingsEditor,
   CustomPauseScreen,
   EditorScreen,
+  EmojiEventsWhiteImage,
   GamepadWhiteImage,
   KeyboardWhiteImage,
   PauseScreenButton,
@@ -19,6 +21,7 @@ import {
   SettingsAppWhiteImage,
   A5200Background,
   TEXT_IDS,
+  achievements,
 } from '@webrcade/app-common';
 
 export class EmulatorPauseScreen extends Component {
@@ -35,9 +38,11 @@ export class EmulatorPauseScreen extends Component {
     GENPLUSGX_SETTINGS: 'genplusgx-settings',
     CHEATS: 'cheats',
     STATE: 'state',
+    ACHIEVEMENTS: 'achievements',
   };
 
   ADDITIONAL_BUTTON_REFS = [React.createRef(), React.createRef(), React.createRef(), React.createRef()];
+  SECONDARY_BUTTON_REFS = [React.createRef(), React.createRef()];
 
   componentDidMount() {
     const { loaded } = this.state;
@@ -57,7 +62,7 @@ export class EmulatorPauseScreen extends Component {
   }
 
   render() {
-    const { ADDITIONAL_BUTTON_REFS, ModeEnum } = this;
+    const { ADDITIONAL_BUTTON_REFS, SECONDARY_BUTTON_REFS, ModeEnum } = this;
     const {
       appProps,
       closeCallback,
@@ -74,6 +79,7 @@ export class EmulatorPauseScreen extends Component {
 
     const additionalButtons = [
       <PauseScreenButton
+        key="controls"
         imgSrc={GamepadWhiteImage}
         buttonRef={ADDITIONAL_BUTTON_REFS[0]}
         label={Resources.getText(TEXT_IDS.VIEW_CONTROLS)}
@@ -85,6 +91,7 @@ export class EmulatorPauseScreen extends Component {
         }}
       />,
       <PauseScreenButton
+        key="settings"
         imgSrc={SettingsAppWhiteImage}
         buttonRef={ADDITIONAL_BUTTON_REFS[1]}
         label="Atari 5200 Settings"
@@ -97,14 +104,36 @@ export class EmulatorPauseScreen extends Component {
       />,
     ]
 
-    if (emulator.getCheatsService().getList().length > 0) {
+    if (cloudEnabled) {
       additionalButtons.push(
         <PauseScreenButton
-          imgSrc={BoltWhiteImage}
+          key="state"
+          imgSrc={SaveWhiteImage}
           buttonRef={ADDITIONAL_BUTTON_REFS[2]}
-          label="Cheats"
+          label={Resources.getText(TEXT_IDS.SAVE_STATES)}
           onHandlePad={(focusGrid, e) =>
             focusGrid.moveFocus(e.type, ADDITIONAL_BUTTON_REFS[2])
+          }
+          onClick={() => {
+            this.setState({ mode: ModeEnum.STATE });
+          }}
+        />
+      );
+    }
+
+    const secondaryButtons = [];
+    let secondaryRefIdx = 0;
+
+    if (emulator.getCheatsService().getList().length > 0) {
+      const cheatsRef = SECONDARY_BUTTON_REFS[secondaryRefIdx++];
+      secondaryButtons.push(
+        <PauseScreenButton
+          key="cheats"
+          imgSrc={BoltWhiteImage}
+          buttonRef={cheatsRef}
+          label="Cheats"
+          onHandlePad={(focusGrid, e) =>
+            focusGrid.moveFocus(e.type, cheatsRef)
           }
           onClick={() => {
             this.setState({ mode: ModeEnum.CHEATS });
@@ -113,21 +142,25 @@ export class EmulatorPauseScreen extends Component {
       );
     }
 
-    if (cloudEnabled) {
-      additionalButtons.push(
+    if (achievements.isLoggedIn() && achievements.hasAchievements()) {
+      const achievementsRef = SECONDARY_BUTTON_REFS[secondaryRefIdx++];
+      secondaryButtons.push(
         <PauseScreenButton
-          imgSrc={SaveWhiteImage}
-          buttonRef={ADDITIONAL_BUTTON_REFS[3]}
-          label={Resources.getText(TEXT_IDS.SAVE_STATES)}
+          key="achievements"
+          imgSrc={EmojiEventsWhiteImage}
+          buttonRef={achievementsRef}
+          label="Achievements"
           onHandlePad={(focusGrid, e) =>
-            focusGrid.moveFocus(e.type, ADDITIONAL_BUTTON_REFS[3])
+            focusGrid.moveFocus(e.type, achievementsRef)
           }
           onClick={() => {
-            this.setState({ mode: ModeEnum.STATE });
+            this.setState({ mode: ModeEnum.ACHIEVEMENTS });
           }}
         />
       );
     }
+
+    const usedSecondaryRefs = SECONDARY_BUTTON_REFS.slice(0, secondaryRefIdx);
 
     const gamepad = <GamepadControlsTab emulator={emulator} />;
     const gamepadLabel = Resources.getText(TEXT_IDS.GAMEPAD_CONTROLS);
@@ -143,6 +176,8 @@ export class EmulatorPauseScreen extends Component {
             isStandalone={isStandalone}
             additionalButtonRefs={ADDITIONAL_BUTTON_REFS}
             additionalButtons={additionalButtons}
+            secondaryButtonRefs={usedSecondaryRefs}
+            secondaryButtons={secondaryButtons}
           />
         ) : null}
         {mode === ModeEnum.CONTROLS ? (
@@ -180,6 +215,11 @@ export class EmulatorPauseScreen extends Component {
             emulator={emulator}
             onClose={closeCallback}
             showStatusCallback={emulator.saveMessageCallback}
+          />
+        ) : null}
+        {mode === ModeEnum.ACHIEVEMENTS ? (
+          <AchievementsScreen
+            onClose={closeCallback}
           />
         ) : null}
       </>
